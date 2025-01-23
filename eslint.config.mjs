b0595d08e493f8c56470c997497db75fd1c42b77
David Melgarejo <davidarcosm@hotmail.com>
2025-01-23 11:53:23 -0600

    Algunas reglas configuradas de eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,20 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      // Ignorar advertencia sobre el uso de <img>
+      "@next/next/no-img-element": "off",
+      // Ignorar el uso de 'any'
+      "@typescript-eslint/no-explicit-any": "off",
+      // Ignorar interfaces vacías
+      "@typescript-eslint/no-empty-object-type": "off",
+      // Ignorar variables no utilizadas
+      "@typescript-eslint/no-unused-vars": "off",
+      // Ignorar preferencia por @ts-expect-error
+      "@typescript-eslint/ban-ts-comment": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
